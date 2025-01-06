04baa3991306c13022efb607a85f62a10f295572
victoria <piggivicki@gmail.com>
2025-01-06 13:36:47 -0500

    initial drop

---
+++ Sabatoria Connect Four/src/sanatorium/ConnectFourGUIDriver.java
@@ -0,0 +1,20 @@
+package sanatorium;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ConnectFourGUIDriver extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+
+	}
+
+}
