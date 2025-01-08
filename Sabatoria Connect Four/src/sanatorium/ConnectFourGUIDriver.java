016c9e320a198f7f9f59357e80a7f3c460a5d7f4
Sana Shahid <sanashahid2134@gmail.com>
2025-01-08 13:21:37 -0500

    update

--- Sabatoria Connect Four/src/sanatorium/ConnectFourGUIDriver.java
+++ Sabatoria Connect Four/src/sanatorium/ConnectFourGUIDriver.java
@@ -9,7 +9,7 @@ public class ConnectFourGUIDriver extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		// TODO Auto-generated method stub
+		
 		stage.show();
 	}
 
