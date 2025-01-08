eddda4bd4634e86184fae5e548f418929321ea74
Sana Shahid <sanashahid2134@gmail.com>
2025-01-08 14:25:44 -0500

    displaying the connect 4 board and a menu  method

--- Sabatoria Connect Four/src/sanatorium/ConnectFourTextDriver.java
+++ Sabatoria Connect Four/src/sanatorium/ConnectFourTextDriver.java
@@ -1,21 +1,33 @@
 package sanatorium;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ConnectFourTextDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		char[][] boardArray = {{'O','O','O','O','O','O'},{'O','O','O','O','O','O'},{'O','O','O','O','O','O'},{'O','O','O','O','O','O'},{'O','O','O','O','O','O'},{'O','O','O','O','O','O'},{'O','O','O','O','O','O'}};
+		menu();
+		Scanner in = new Scanner(System.in);
+	
 		String[][] boardArray2 = {{"O","O","O","O","O","O","O"},{"O","O","O","O","O","O","O"},{"O","O","O","O","O","O","O"},{"O","O","O","O","O","O","O"},{"O","O","O","O","O","O","O"},{"O","O","O","O","O","O","O"},{"O","O","O","O","O","O","O"}};
-		
+
+		for (int i = 0; i< boardArray2.length-1; i++ ) {
+		System.out.println(Arrays.toString(boardArray2[1]));}
+
+	}
+	
+	public static void menu() {
 		
 		System.out.println("Connect Four");
 		System.out.println("--------------");
 		System.out.println("                ");
-		for (int i = 0; i< boardArray2.length-1; i++ ) {
-		System.out.println(Arrays.toString(boardArray2[1]));}
+		System.out.println("How to play: ");
+		System.out.println("Decide what colour you would like to be");
+		System.out.println("Enter the position of where you would like to drop your colour token ");
+		System.out.println("Try to get 4 of your coloured tokens in a row, either horizontally, vertacally or diagonally");
+		System.out.println("                ");
 	}
 
 }
+
+
