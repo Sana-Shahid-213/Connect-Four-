ff6a3d8a8d5ef24e85255b62ef0af291d41d16ab
victoria <piggivicki@gmail.com>
2025-01-09 23:44:17 -0500

    minor reorganization

--- Sabatoria Connect Four/src/sanatorium/ConnectFourTextDriver.java
+++ Sabatoria Connect Four/src/sanatorium/ConnectFourTextDriver.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class ConnectFourTextDriver {
 
 	public static void main(String[] args) {
-		menu();
+		
 		Scanner in = new Scanner(System.in);
 		int column = 0;
 		int row;
@@ -16,10 +16,15 @@ public class ConnectFourTextDriver {
 				{ "O", "O", "O", "O", "O", "O", "O" }, { "O", "O", "O", "O", "O", "O", "O" },
 				{ "O", "O", "O", "O", "O", "O", "O" }, { "O", "O", "O", "O", "O", "O", "O" },
 				{ "O", "O", "O", "O", "O", "O", "O" } };
-
-		for (int i = 0; i < boardArray2.length - 1; i++) {
-			System.out.println(Arrays.toString(boardArray2[i]));
-		}
+		
+		
+		//start time
+		menu();
+		printBoard(boardArray2);
+		
+		//for (int i = 0; i < boardArray2.length - 1; i++) {
+			//System.out.println(Arrays.toString(boardArray2[i]));
+		//}
 		// prompting user input
 		// System.out.println(" ");
 
@@ -138,7 +143,7 @@ public class ConnectFourTextDriver {
 		System.out.println("Decide what colour you would like to be");
 		System.out.println("Enter the position of where you would like to drop your colour token ");
 		System.out.println(
-				"Try to get 4 of your coloured tokens in a row, either horizontally, vertacally or diagonally");
+				"Try to get 4 of your coloured tokens in a row, either horizontally, vertically or diagonally");
 		System.out.println("Player 1 is Red represented by R");
 		System.out.println("Player 2 is Yellow represented by Y");
 		System.out.println("  ");
