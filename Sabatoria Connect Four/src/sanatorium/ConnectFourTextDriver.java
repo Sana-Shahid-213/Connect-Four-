74cf830dc9349f5caba11fdd4e99d61a9ffeb9a2
victoria <piggivicki@gmail.com>
2025-01-09 23:03:27 -0500

    Implemented drop mechanics in the text based version of Connect Four,
    created methods to print the connect four board and implemented a turn
    taking system for players to place tokens of alternating colours.

--- Sabatoria Connect Four/src/sanatorium/ConnectFourTextDriver.java
+++ Sabatoria Connect Four/src/sanatorium/ConnectFourTextDriver.java
@@ -11,7 +11,7 @@ public class ConnectFourTextDriver {
 		int column = 0;
 		int row;
 		boolean playerOneTurn = true;
-		//
+		// connect 4 board as a 2D Array
 		String[][] boardArray2 = { { "O", "O", "O", "O", "O", "O", "O" }, { "O", "O", "O", "O", "O", "O", "O" },
 				{ "O", "O", "O", "O", "O", "O", "O" }, { "O", "O", "O", "O", "O", "O", "O" },
 				{ "O", "O", "O", "O", "O", "O", "O" }, { "O", "O", "O", "O", "O", "O", "O" },
@@ -20,36 +20,79 @@ public class ConnectFourTextDriver {
 		for (int i = 0; i < boardArray2.length - 1; i++) {
 			System.out.println(Arrays.toString(boardArray2[i]));
 		}
-		//prompting user input
-		System.out.println("                ");
+		// prompting user input
+		// System.out.println(" ");
 
-		System.out.println("Enter the column number (0-6):");
-		column = in.nextInt();
+		// System.out.println("\nEnter the column number (0-6):");
+		// column = in.nextInt();
+		// in.nextLine();
+		/*
+		 * System.out.println("Enter the row number (0-5): "); row = in.nextInt();
+		 * 
+		 * boardArray2[row][column] = "R"; for (int i = 0; i < boardArray2.length - 1;
+		 * i++) { System.out.println(Arrays.toString(boardArray2[i])); }
+		 */
 
-		System.out.println("Enter the row number (0-5): ");
-		row = in.nextInt();
+		// implementing dropping
 
-		boardArray2[column][row] = "R";
-		for (int i = 0; i < boardArray2.length - 1; i++) {
-			System.out.println(Arrays.toString(boardArray2[i]));
+		// back up 1
+		// drop(column, boardArray2);
+		// printBoard(boardArray2);
+
+		boolean gameOver = false;
+		String currentPlayer = "Red";
+
+		// looping to let players alternate placing tokens
+		while (gameOver == false) {
+			System.out.println("\n" + currentPlayer + ", enter a column number from values 1-7");
+			column = in.nextInt() - 1;
+			in.nextLine();
+
+			drop(column, currentPlayer, boardArray2);
+			printBoard(boardArray2);
+			// switching player
+			if (currentPlayer == "Red") {
+				currentPlayer = "Yellow";
+			} else {
+				currentPlayer = "Red";
+			}
 		}
-		
-		//implementing dropping
-		
-		//back up 1
 
 	}
-	public static void drop(int col, int row, String[][] modArray){
-		if (Arrays.toString(modArray[col])=="0") {
-			
+
+	public static void drop(int column, String player, String[][] modArray) {
+		boolean placed = false;
+
+		for (int i = 6; i >= 0; i--) {
+
+			// if the space is free, add a token
+			if (modArray[i][column] == "O" && placed == false) {
+
+				// System.out.println("it worked!");
+
+				// depending on the current player, place a different colour of token
+				if (player == "Yellow") {
+					modArray[i][column] = "Y";
+
+				} else if (player == "Red") {
+					modArray[i][column] = "R";
+
+				}
+				// System.out.println("column" + column + "row" + i);
+				placed = true;
+			}
 		}
-		//for (int i = 0; i < boardArray2.length - 1; i++) {
-			//System.out.println(Arrays.toString(boardArray2[i]));
 	}
-	
-	/**
-	 * Purpose: to display the menu of the 
-	 */
+
+	// prints the updated board.
+	public static void printBoard(String[][] board) {
+		for (int j = 0; j < board.length; j++) {
+			System.out.println(Arrays.toString(board[j]));
+			// System.out.println("row " + j);
+		}
+	}
+
+	//prints menu
 	public static void menu() {
 
 		System.out.println("Connect Four");
